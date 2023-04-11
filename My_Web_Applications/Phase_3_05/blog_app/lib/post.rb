0ec9fb3911eacd98badf9164dd869a2bd7cf4ef8
Chris Toplisek <christoplisek@MASTUDENT-Chris-Toplisek-C17GMAZMQ6L4.local>
2023-04-11 13:38:05 +0100

    project setup

--- My_Web_Applications/Phase_3_05/blog_app/lib/post.rb
+++ My_Web_Applications/Phase_3_05/blog_app/lib/post.rb
@@ -1,7 +1,7 @@
 class Post
   attr_accessor :title, :content, :tags
 
-  def initialize(title, content, tags)
+  def initialize(title, content, tags = [])
     @title = title
     @content = content
     @tags = tags
